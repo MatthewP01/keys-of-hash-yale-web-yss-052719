96e29c9c79f82a500e068cc9b1c668ebd16b53d5
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-22 22:19:05 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,12 +1,28 @@
+# class Hash
+#   def keys_of(*arguments)
+#     array_output = []
+#     self.each do |key, value|
+#       arguments.each do |place|
+#         if place == value
+#           array_output  << key
+#         end
+#       end
+#     end
+#   return array_output
+# end
 class Hash
   def keys_of(*arguments)
-    array_output = []
+    array = []
     self.each do |key, value|
-      arguments.each do |place|
-        if place == value
-          array_output  << key
+      arguments.each do |i|
+        if i == value
+        array.push(key)
         end
-      end
+      end 
+
+
     end
-  return array_output
-end
+    return array
+  end
+  
+end 
